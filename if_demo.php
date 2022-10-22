18a8e15308102bf35096e3cb71e255ff614b4d91
Joemar Recelestino <recelestino90@gmail.com>
2022-10-22 16:16:04 +0800

    Done

---
+++ if_demo.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Decision Making</title>
+</head>
+<body>
+    <?php
+
+        // IF ELSEIF ELSE STATEMENT
+        // if (condition) {
+        //     code to be executed if condition is true;
+        // } 
+
+        // if (condition) {
+        //     code to be executed if condition is true;
+        // } 
+        // else {
+        //     code to be executed if condition is false;
+        // } 
+
+        // if (condition) {
+        //     code to be executed if this condition is true;
+        // } 
+        // elseif (condition) {
+        //   code to be executed if first condition is false and this condition is true;
+        // } 
+        // else {
+        //   code to be executed if all conditions are false;
+        // } 
+
+        //SWITCH CASE STATEMENT 
+        // switch (n) {
+        //   case label1:
+        //     code to be executed if n=label1;
+        //     break;
+        //   case label2:
+        //     code to be executed if n=label2;
+        //     break;
+        //   case label3:
+        //     code to be executed if n=label3;
+        //     break;
+        //     ...
+        //   default:
+        //     code to be executed if n is different from all labels;
+        // } 
+
+    ?>
+</body>
+</html>
