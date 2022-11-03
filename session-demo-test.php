fb4c8ac0f14f531021a671c3cb131e8f3d0ed161
joewmar <recelestino90@gmail.com>
2022-11-03 23:19:10 +0800

    DONE PHP Lesson 11.2

---
+++ session-demo-test.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Session Demo Test</title>
+</head>
+<body>
+    <?php
+    if(isset($_SESSION['username'])){
+        echo $_SESSION['username']; 
+        echo $_SESSION['firstname'];
+        echo $_SESSION['lastname'];
+    }
+    ?>
+</body>
+</html>
